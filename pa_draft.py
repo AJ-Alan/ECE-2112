681818df7e21a9dfce05dd6d2f830d8764c329dd
AJ <124542692+AJ-Alan@users.noreply.github.com>
2024-08-25 22:41:26 +0800

    Update pa_draft.py

--- pa_draft.py
+++ pa_draft.py
@@ -1,3 +1,4 @@
+#Alphabet Soup Problem#
 #Create a function that takes a string and returns a string with its letters in alphabetical order
 
 def alphabet_soup(s): #define function "alphabet_soup" that sorts the letters of input string where the input is the variable s
@@ -9,6 +10,8 @@ def alphabet_soup(s): #define function "alphabet_soup" that sorts the letters of
 s = str(input("\nEmoticon converter\nEnter a word:")) #sets the variable s as a string input
 alphabet_soup(s) #calls function "alphabet_soup"
 
+
+#Emoticon Problem#
 #Create a function that changes specific words into emoticons
 #Smile -> :) , Grin -> :D , Sad -> :(( , Mad -> >:(, lmao -> XD ##added lmao for me XD
 
@@ -35,3 +38,22 @@ def chaEmo(s,word,emoticon): #generalized function that changes the values
 #starts main function
 s = str(input("Enter a sentence: ")) #sets the input string to s
 emotify(s) #calls function emotify with input s
+
+
+#Unpacking List Problem#
+#Create a function that unpacks the list [write your code here] into three variables: first, middle, and last
+#where the first and last are the respective first and last values of the list and the middle is everything else in between
+#Then print all three variables
+
+def unpack(s): #define function "unpack" which will create 3 variables from input s and prints them out
+    first = s[0] #sets the "first" variable to be equal to the first element of the list
+    last = s[len(s)-1] #sets the "last" variable to be equal to the last element of the list
+    s.remove(s[0]) #removes the first element
+    s.remove(s[len(s)-1]) #removes the last element
+    middle = s #sets the "middle" variable to be the rest of the elements of the list
+    print("first: ",first,"\tmiddle: ",middle,"\tlast: ", last) #prints out the elements of the list
+
+#starts main function
+s = input("Please input a list and separate it with commas: ") #sets the input string to s
+s = s.split(",") #splits s into a list with "," as the separator
+unpack(s) #calls function unpack with input s
