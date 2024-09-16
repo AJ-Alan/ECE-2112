e06454a3642c36d4c87bdca05e3703e81c76792f
AJ <124542692+AJ-Alan@users.noreply.github.com>
2024-09-16 23:36:18 +0800

    Update Villarmil_Pandas-P1.py

--- Villarmil_Pandas-P1.py
+++ Villarmil_Pandas-P1.py
@@ -1,3 +1,3 @@
 import pandas as pd #import pandas
 dfCars = pd.read_csv('cars.csv') #reads the file 'cars.csv' and inserts to variable 'dfCars'
-dfCars.loc[(dfCars.index<5)|(dfCars.index>26)] #displays the first five rows and the last five rows of the dataframe
+print(dfCars.loc[(dfCars.index<5)|(dfCars.index>26)]) #displays the first five rows and the last five rows of the dataframe
